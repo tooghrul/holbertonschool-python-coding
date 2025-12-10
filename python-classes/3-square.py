bca5575869ea7b32c572ae2dc1da95031af47ad9
T <t@abc.com>
2025-12-10 09:47:31 +0000

    Done.

---
+++ python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module is a Square class with a size attribute and an area method."""
+
+
+class Square:
+    """Represents a square with a size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a given size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size**2
+
+    @property
+    """Retrieves size attribute"""
+    def size(self):
+        return self.__size
+
+    @size.setter
+    """It makes sure that the value is integer and not negative"""
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
