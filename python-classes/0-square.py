2ee37d402fb2a89eff0351ee648978215c19c030
T <t@abc.com>
2025-12-10 09:28:27 +0000

    Done.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This class us about square and it has 'size' attribute."""
 class Square:
+    """Represents a square with size"""
     def __init__(self, size):
         self.__size = size
