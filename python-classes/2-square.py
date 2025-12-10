f0ce2a2968eb4466755f1cc1aa0859d6d79a12ed
T <t@abc.com>
2025-12-10 09:40:13 +0000

    Done.

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This class us about square and it has 'size' attribute."""
+
+
+class Square:
+    """Represents a square with size"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    @property
+    def area(self):
+        return self.__size**2
